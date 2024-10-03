ae894d7bda2b7d1612c56e88eb5307ae27703232
raj singh <rajsingh@rajs-MacBook-Air.local>
2024-10-03 10:02:26 +0530

    updated

---
+++ TODO.sql
@@ -0,0 +1,17 @@
+
+
+##### Things Happening 
+
+1. Understanding of Snowflke : DONE 
+2. Deploying Streamlit Application in Snowflake : DONE 
+3. Understanding of Cortex Model: DONE 
+
+
+3. Uderstanding of LangChain : In-progress
+4. Understading of LangGraph: IN-Progress 
+5. Cost Optimization USing snowflake : yet to Start 
+    https://docs.snowflake.com/en/user-guide/cost-understanding-overall
+    1. Understanding Compute Cost 
+    2. Understanding Storage Cost 
+    3. Understanding Data Transfer cost 
+
